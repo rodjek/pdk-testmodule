965593119d795fe38d5f301f2a1d2029f56ddf62
Tim Sharpe <tim@sharpe.id.au>
2017-08-29 12:17:17 +1000

    Add test class

---
+++ manifests/test.pp
@@ -0,0 +1,10 @@
+# testmodule::test
+#
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include testmodule::test
+class testmodule::test {
+}
